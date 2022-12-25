a53af9d7bfb089f033377a057efee3d2ab094704
hirap <hhirashima729@gmail.com>
2022-12-26 00:46:39 +0900

    show shared_ptr user's number

--- item14/item14.cpp
+++ item14/item14.cpp
@@ -15,12 +15,12 @@ class myMutex{
         {}
 };
 
-void lock(myMutex *pm)
+void lock(myMutex* pm)
 {
     pm->counter++;
 }
 
-void unlock(myMutex *pm)
+void unlock(myMutex* pm)
 {
     pm->counter--;
     std::cout << "counter status " << pm->counter << std::endl;
@@ -28,26 +28,32 @@ void unlock(myMutex *pm)
 
 class Lock{
     public:
-        explicit Lock(myMutex *pm)
+        explicit Lock(myMutex* pm)
         :mutexPtr(pm, unlock)
         {
-            lock(pm);
+            lock(mutexPtr.get());
         }
+
+        void getUserCount()
+        {
+            std::cout << "myMutex user_count:" << mutexPtr.use_count() << std::endl;
+        }
+
     private:
         std::shared_ptr<myMutex> mutexPtr;
 };
 
 int main(void)
 {
-    myMutex *main_pm;
-
-    main_pm = new myMutex();
+    myMutex *main_pm = new myMutex();
 
     Lock lock1(main_pm);
     std::cout << "lock1 " << main_pm->counter << std::endl;
+    lock1.getUserCount();
 
-    Lock lock2(main_pm);
+    Lock lock2(lock1);
     std::cout << "lock2 " << main_pm->counter << std::endl;
+    lock2.getUserCount();
 
     return 0;
 }
